09e2aa0b930c4f382b43d15839e97db6688149c3
Vito Valov <vitovalov@gmail.com>
2017-05-22 20:04:02 +0200

    sugue on ipad works

---
+++ FaceIt/EmotionsViewController.swift
@@ -0,0 +1,32 @@
+//
+//  EmotionsViewController.swift
+//  FaceIt
+//
+//  Created by Vito on 22/05/2017.
+//  Copyright © 2017 Vito. All rights reserved.
+//
+
+import UIKit
+
+class EmotionsViewController: UIViewController {
+    
+    // MARK: - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+        let destinationViewController = segue.destination
+        if let faceViewController = destinationViewController as? FaceViewController,
+            let identifier = segue.identifier,
+            let expression = emotionalFaces[identifier] {
+                faceViewController.expression = expression
+        }
+    }
+    
+    private let emotionalFaces: Dictionary<String, FacialExpression> = [
+        "sad": FacialExpression(eyes: .closed, mouth: .frown),
+        "happy": FacialExpression(eyes: .open, mouth: .smile),
+        "worried": FacialExpression(eyes: .open, mouth: .smirk)
+    ]
+}
