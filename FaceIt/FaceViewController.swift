b34dd8b7081653dfdd5579ee15c4376ec82f0a23
Vito Valov <vitovalov@gmail.com>
2017-05-29 12:20:40 +0200

    animating head shake by tap

--- FaceIt/FaceViewController.swift
+++ FaceIt/FaceViewController.swift
@@ -9,15 +9,15 @@
 import UIKit
 
 class FaceViewController: VCLLoggingViewController {
-
+    
     @IBOutlet weak var faceView: FaceView! {
         didSet { // gonna happen when ios hooks up to the view, only once
             let handler = #selector(FaceView.changeScale(byReactingTo:))
             let pinchRecognizer = UIPinchGestureRecognizer(target: faceView, action: handler)
             faceView.addGestureRecognizer(pinchRecognizer)
-            let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(toggleEyes(byReactingTo:)))
-            tapRecognizer.numberOfTapsRequired = 1
-            faceView.addGestureRecognizer(tapRecognizer)
+//            let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(toggleEyes(byReactingTo:)))
+//            tapRecognizer.numberOfTapsRequired = 1
+//            faceView.addGestureRecognizer(tapRecognizer)
             
             let swipeUpRecognizer = UISwipeGestureRecognizer(target: self, action: #selector(increasseHappiness))
             swipeUpRecognizer.direction = .up
@@ -28,6 +28,44 @@ class FaceViewController: VCLLoggingViewController {
             faceView.addGestureRecognizer(swipeDownRecognizer)
             updateUI()
         }
+    }
+    
+    private struct HeadShake {
+        static let angle = CGFloat.pi/6 // radians
+        static let segmentDuration: TimeInterval = 0.5 //each head shake has 3 segments
+    }
+    
+    private func rotateFace(by angle: CGFloat) {
+        faceView.transform = faceView.transform.rotated(by: angle)
+    }
+    
+    private func shakeHead() {
+        UIView.animate(
+            withDuration: HeadShake.segmentDuration,
+            animations: { self.rotateFace(by: HeadShake.angle) }, // we're in middle of animation, no need to break mem cycle
+            completion: { finished in
+                if finished {
+                    UIView.animate(
+                        withDuration: HeadShake.segmentDuration,
+                        animations: { self.rotateFace(by: -HeadShake.angle*2) },
+                        completion: { finished in
+                            if finished {
+                                UIView.animate(
+                                    withDuration: HeadShake.segmentDuration,
+                                    animations: { self.rotateFace(by: HeadShake.angle) }
+                                )
+                            }
+                    }
+                        
+                        
+                    )
+                }
+                
+        })
+    }
+    
+    @IBAction func shakeHead(_ sender: Any) {
+        shakeHead()
     }
     
     func increasseHappiness()
@@ -46,7 +84,7 @@ class FaceViewController: VCLLoggingViewController {
             expression = FacialExpression(eyes: eyes, mouth: expression.mouth)
         }
     }
-  
+    
     var expression = FacialExpression(eyes: .closed, mouth: .frown) {
         didSet { // when we initialize something, didSet doesn't get called.
             updateUI()
